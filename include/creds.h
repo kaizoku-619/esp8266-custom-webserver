3e2a3176afeeabc42e6d3facdd10bc2fbeb8b075
Bayrem Gharssellaoui <garssallaoui.bayrem@gmail.com>
2019-01-11 09:45:06 -0600

    added include directory

---
+++ include/creds.h
@@ -0,0 +1,7 @@
+// WiFi credentials
+#define SSID "your wifi ssid"
+#define PASSWORD "your wifi name"
+
+// Login credentials
+#define USERNAME "your login name"
+#define USER_PASSWORD "your login password"
